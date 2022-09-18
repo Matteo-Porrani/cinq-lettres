9837384530b3cf1f145471a6c3646072cfbcb310
pro16 <matteo.porrani@gmail.com>
2022-09-18 23:02:42 +0200

    ajout paroles

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -11,15 +11,15 @@
                 </template>
             </MainLink>
 
-            <h2 class="fs-5 text-center mb-3">Liste des mots</h2>
+            <h2 class="fs-5 text-center text-primary mb-5">Liste des mots</h2>
 
 <!--            <p class="f-inria text-mid fs-3 text-center">Voici la liste des mots<br>disponibles à ce jour :</p>-->
-            <p class="f-inria text-mid fs-3 text-center mb-3 ">
+            <p class="f-inria text-mid fs-3 text-center">
                 En panne d'inspiration ?
             </p>
 
-            <p class="f-inria text-mid fs-2 text-center">
-                Voici la liste des mots<br>disponibles à ce jour :
+            <p class="f-inria text-mid fs-3 text-center">
+                Voici les mots disponibles à ce jour :
             </p>
 
             <WordListing/>
