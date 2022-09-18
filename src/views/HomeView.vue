26742f2e1e5e91e2984af448f055554048bed1a6
pro16 <matteo.porrani@gmail.com>
2022-09-18 22:45:44 +0200

    Update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,13 @@
     <div class="home">
         <div class="container">
 
-            <h1 class="text-center fs-8 mt-5">PENTA</h1>
+            <h1 class="text-center text-accent-light fs-12 mt-5" style="z-index: 9999;">PENTA</h1>
+            <h1 class="text-center fs-12"
+                style="transform: translate(-1%, -111%);">
+                PENTA
+            </h1>
 
-            <h2 class="f-inria text-center mt-4" style="font-weight: normal;">Entre vous et la victoire,<br>seulement 5 petites lettres !</h2>
+            <h2 class="f-inria text-center mt-2" style="font-weight: normal;">Entre vous et la victoire,<br>seulement 5 petites lettres !</h2>
 <!--            <h2 class="f-inria text-center mt-4" style="font-weight: normal;">Combien de mots de 5 lettres connaissez-vous ?</h2>-->
 
             <MainNavigation/>
